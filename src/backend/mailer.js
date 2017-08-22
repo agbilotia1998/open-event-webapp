6085c6055d29e2f1b2e6645e861a267102dea5a4
Ayush Gupta <iec2016039@iiita.ac.in>
2017-08-22 20:19:01 +0530

    Added error in mail

--- src/backend/mailer.js
+++ src/backend/mailer.js
@@ -1,7 +1,4 @@
-/**
- * Created by championswimmer on 1/8/16.
- * Modified by uttpal.
- */
+
 'use strict';
 const config = require('../../config.json');
 
@@ -34,123 +31,134 @@ let s3 = new aws.S3();
  * Support Google Cloud Store too using the same AWS SDK via Google Cloud Storage interoperability keys
  */
 if (process.env.CLOUD_STORAGE === 'google_cloud') {
-  s3 = new aws.S3({endpoint: new aws.Endpoint('https://storage.googleapis.com')});
+    s3 = new aws.S3({endpoint: new aws.Endpoint('https://storage.googleapis.com')});
 }
 
 process.env.DEFAULT_MAIL_STRATEGY = process.env.DEFAULT_MAIL_STRATEGY || config.DEFAULT_MAIL_STRATEGY;
 process.env.DEFAULT_FROM_EMAIL = process.env.DEFAULT_FROM_EMAIL || config.DEFAULT_FROM_EMAIL;
 
 function uploadToS3(file, fileName, socket) {
-  const uploadParams = {
-    Bucket: process.env.AWS_BUCKET,
-    Key: fileName
-  };
-  const fileStream = fs.createReadStream(file);
+    const uploadParams = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: fileName
+    };
+    const fileStream = fs.createReadStream(file);
 
-  fileStream.on('error', function(err) {
-    console.log('File Error');
-    logger.addLog('Error', 'Error during upload to S3', socket, err);
-  });
-  uploadParams.Body = fileStream;
-  return s3.upload(uploadParams).promise();
+    fileStream.on('error', function(err) {
+        console.log('File Error');
+        logger.addLog('Error', 'Error during upload to S3', socket, err);
+    });
+    uploadParams.Body = fileStream;
+    return s3.upload(uploadParams).promise();
 }
 
 function emailSend(toEmail, url, appName) {
-  const subject = 'Your webapp ' + appName + ' is Ready';
-  let downloadUrl = url;
-  const previewUrl = appUrl + 'live/preview/' + toEmail + '/' + encodeURIComponent(appName);
-  const currDate = new Date();
-  let previewExpiryDate = new Date();
-  let downloadLinkExpiryDate = new Date();
+    const subject = 'Your webapp ' + appName + ' is Ready';
+    let downloadUrl = url;
+    const previewUrl = appUrl + 'live/preview/' + toEmail + '/' + encodeURIComponent(appName);
+    const currDate = new Date();
+    let previewExpiryDate = new Date();
+    let downloadLinkExpiryDate = new Date();
+    let uploadFailureMesg = '';
+    let mailSendStatus = 0; // boolean to indicate the success and failure of email send
 
-  previewExpiryDate.setTime(currDate.getTime() + 7200000); // corresponds to current date + 2 hours
-  downloadLinkExpiryDate.setTime(currDate.getTime() + 259200000); // corresponds to current date + 3 days
-  previewExpiryDate = moment.utc(previewExpiryDate).local().format('dddd, h A');
-  downloadLinkExpiryDate = moment.utc(downloadLinkExpiryDate).local().format('dddd, MMMM Do YYYY');
+    previewExpiryDate.setTime(currDate.getTime() + 7200000); // corresponds to current date + 2 hours
+    previewExpiryDate = moment.utc(previewExpiryDate).local().format('dddd, h A');
 
-  if(!url) {
+
+    // The Cloud upload fails so download link point to the event folder on Heroku itself which will be purged in a few hours
+ if(url){
     downloadUrl = appUrl + 'download/' + toEmail + '/' + appName;
-  }
-  const emailContent = 'Hi ! <br>' +
+    downloadLinkExpiryDate.setTime(currDate.getTime() + 7200000); // corresponds to current date + 2 hours
+    uploadFailureMesg = '. There was some problem with the cloud upload of the event folder. Please download the zip immediately. It will be purged soon from the generator server';
+}
+else {
+    downloadLinkExpiryDate.setTime(currDate.getTime() + 259200000); // corresponds to current date + 3 days
+}
+downloadLinkExpiryDate = moment.utc(downloadLinkExpiryDate).local().format('dddd, MMMM Do YYYY');
+
+const emailContent = 'Hi ! <br>' +
     ' Your webapp has been generated <br>' +
     'You can preview it live on ' + ' link'.link(previewUrl) + '. This link expires on ' + previewExpiryDate + '<br>' +
-    'You can download a zip of your website from  ' + ' here'.link(downloadUrl) + '. The download link for zip expires on ' + downloadLinkExpiryDate +
+    'You can download a zip of your website from  ' + ' here'.link(downloadUrl) + '. The download link for zip expires on ' + downloadLinkExpiryDate + uploadFailureMesg +
     '<br><br><br>' +
     'Thank you for using Open Event Webapp Generator :)';
 
-  if(process.env.DEFAULT_MAIL_STRATEGY === 'SMTP') {
+if(process.env.DEFAULT_MAIL_STRATEGY === 'SMTP') {
     const smtpConfig = {
-      host: process.env.SMTP_HOST || config.SMTP_HOST,
-      port: process.env.SMTP_PORT || config.SMTP_PORT,
-      auth: {
-        user: process.env.SMTP_USERNAME || config.SMTP_USERNAME,
-        pass: process.env.SMTP_PASSWORD || config.SMTP_PASSWORD
-      }
+        host: process.env.SMTP_HOST || config.SMTP_HOST,
+        port: process.env.SMTP_PORT || config.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USERNAME || config.SMTP_USERNAME,
+            pass: process.env.SMTP_PASSWORD || config.SMTP_PASSWORD
+        }
     };
     const transporter = nodemailer.createTransport(smtpTransport(smtpConfig));
 
     return transporter.sendMail({
-      from: process.env.DEFAULT_FROM_EMAIL,
-      to: toEmail,
-      subject: subject,
-      html: emailContent
+        from: process.env.DEFAULT_FROM_EMAIL,
+        to: toEmail,
+        subject: subject,
+        html: emailContent
     }).then((response) => {
-      console.log('Successfully sent Email');
-      console.log(response);
-      return url;
+        mailSendStatus = 1;
+        console.log('Successfully sent Email');
+        console.log(response);
+        return {'url': url, 'mail': mailSendStatus};
     }).catch((err) => {
-      console.log('Error sending Email', err);
-      return url;
-      });
-  }
-  const fromEmail = new helper.Email(process.env.DEFAULT_FROM_EMAIL);
-  const recipientEmail = new helper.Email(toEmail);
-  const content = new helper.Content('text/html', emailContent);
-  const mail = new helper.Mail(fromEmail, subject, recipientEmail, content);
-  const requestBody = mail.toJSON();
-  const request = sg.emptyRequest();
+        console.log('Error sending Email', err);
+        return {'url': url, 'mail': mailSendStatus};
+    });
+}
+const fromEmail = new helper.Email(process.env.DEFAULT_FROM_EMAIL);
+const recipientEmail = new helper.Email(toEmail);
+const content = new helper.Content('text/html', emailContent);
+const mail = new helper.Mail(fromEmail, subject, recipientEmail, content);
+const requestBody = mail.toJSON();
+const request = sg.emptyRequest();
 
-  request.host = 'api.sendgrid.com';
-  request.method = 'POST';
-  request.path = '/v3/mail/send';
-  request.body = requestBody;
-  console.log(request);
-  return sg.API(request)
+request.host = 'api.sendgrid.com';
+request.method = 'POST';
+request.path = '/v3/mail/send';
+request.body = requestBody;
+console.log(request);
+return sg.API(request)
     .then((response) => {
-      console.log(response.statusCode);
-      console.log(response.body);
-      console.log(response.headers);
-      return url;
+        mailSendStatus = 1;
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+        return {'url': url, 'mail': mailSendStatus};
     })
     .catch((err) => {
-      console.log('Error sending Email', err);
-      return url;
+        console.log('Error sending Email', err);
+        return {'url': url, 'mail': mailSendStatus};
     });
 }
 
 function uploadAndsendMail(toEmail, appName, socket, done) {
-  const file = distHelper.distPath + '/' + toEmail + '/event.zip';
+    const file = distHelper.distPath + '/' + toEmail + '/event.zip';
 
-  appName = appName.split(' ').join('_');
-  const fileName = uuid.v4() + '/' + appName + '.zip';
+    appName = appName.split(' ').join('_');
+    const fileName = uuid.v4() + '/' + appName + '.zip';
 
-  uploadToS3(file, fileName, socket)
-    .then((data) => {
-      logger.addLog('Success', 'Upload successful to s3', socket);
-      data.Location = decodeURIComponent(data.Location);
-      console.log('Upload Success', data.Location);
-      return emailSend(toEmail, data.Location, appName);
-    })
-    .catch((err) => {
-      console.log('Error', err);
-      logger.addLog('Info', 'Failed while uploading the zip. No Amazon S3 keys found', socket);
-      return emailSend(toEmail, null, appName);
-    })
-    .then((url) => {
-      return done(url);
-    });
+    uploadToS3(file, fileName, socket)
+        .then((data) => {
+            logger.addLog('Success', 'Upload successful to s3', socket);
+            data.Location = decodeURIComponent(data.Location);
+            console.log('Upload Success', data.Location);
+            return emailSend(toEmail, data.Location, appName);
+        })
+        .catch((err) => {
+            console.log('Error', err);
+            logger.addLog('Info', 'Failed while uploading the zip. No Amazon S3 keys found', socket);
+            return emailSend(toEmail, null, appName);
+        })
+        .then((obj) => {
+            return done(obj);
+        });
 }
 
 module.exports = {
-  uploadAndsendMail
+    uploadAndsendMail
 };
